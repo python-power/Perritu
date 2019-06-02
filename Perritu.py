64d7323517b65d7a1db14c8558a06c50a6eac91a
Kemill <Ckemill@gmail.com>
2019-06-01 23:23:04 -0400

    agregue el estado del bot

--- Perritu.py
+++ Perritu.py
@@ -7,5 +7,6 @@ TOKEN = 'NDYyNDU2MTEwNTk1NDQwNjUw.XPKnfA.Bx4S2NwjMNsbiqCPQH_ukmcTjsI'
 @client.event
 async def on_ready():
 	print("Perritu esta listo para carrear!")
+	await client.change_presence(activity=discord.Game(name='a ser un gato'))
 
 client.run(TOKEN)
